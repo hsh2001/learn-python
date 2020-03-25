ad0f084a3a36e08b9c3754ded517f094f24cfd8b
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-03-25 11:18:16 +0900

    Create 1_variable.py

---
+++ 0000/1_variable.py
@@ -0,0 +1,24 @@
+# 변수!
+
+name = "My name is HSH"
+print(name)
+
+name = "Your name is SMK"
+print(name)
+
+a = 5
+b = 7
+print(a + b)
+print("a + b")
+
+name_first = "Seung hyun"
+name_last = "Hwang"
+
+name_full = name_first + " " + name_last
+print(name_full)
+
+name_full = name_last + " " + name_first
+print(name_full)
+
+# 변수명은 가독성있게 의미 있는 단어로!
+# if, while 같은 예약어는 사용 불가능하다!
