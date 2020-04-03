793ae17d6b265b0300d97fed570b0ee31beaaa60
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-04-03 14:18:43 +0900

    Create array.py

---
+++ 0003/array.py
@@ -0,0 +1,14 @@
+colors = ["빨", "주", "노", "초", "파", "남", "보"]
+
+print(colors[1])
+print(colors[3])
+print(len(colors))
+
+week = ["월", "화", "수", "목", "금", "토", "일"]
+print("주중:", week[:5])
+print("주말:", week[5:])
+print("프로그래밍기초1(11분반):", [week[2], week[4]])
+
+print(week[::-1])
+
+print([1, 2, 3, 4, 5] + [9, 10, 11, 22])
