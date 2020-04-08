8606dacfa07be8f86d23a9ea7820743bff19e14c
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-04-08 10:54:55 +0900

    Update array.py

--- 0003/array.py
+++ 0003/array.py
@@ -12,3 +12,21 @@ print("프로그래밍기초1(11분반):", [week[2], week[4]])
 print(week[::-1])
 
 print([1, 2, 3, 4, 5] + [9, 10, 11, 22])
+print([1, 2, 3] * 3)
+print(2 in [1, 2, 3])
+print(100 in [1, 2, 3])
+
+colors.append('하양')
+print(colors)
+
+colors.extend(['검정', '회색'])
+print(colors)
+
+colors.insert(2, '갈색')
+print(colors)
+
+colors.remove('주')
+print(colors)
+
+del colors[0]
+print(colors)
