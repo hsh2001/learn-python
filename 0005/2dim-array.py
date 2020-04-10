ba8388c3a1e01560a6b22aaed6fcd9d5cb44a6b4
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-04-10 14:11:31 +0900

    Create 2dim-array.py

---
+++ 0005/2dim-array.py
@@ -0,0 +1,14 @@
+kor_score = [49, 79, 20, 100, 80]
+math_score = [43, 59, 85, 20, 90]
+eng_score = [49, 79, 48, 60, 100]
+midterm_score = [kor_score, math_score, eng_score]
+
+print(midterm_score)
+print(midterm_score[0][2])
+
+row1 = [1, 2, 3, 4, 5]
+row2 = [6, 7, 8, 9, 10]
+row3 = [11, 12, 13, 14, 15]
+M = [row1, row2, row3]
+print(M)
+print(M[1][2])
